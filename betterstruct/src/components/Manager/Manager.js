af01bf9a8884932178bd117b06567ec56df7afb6
Saeed Rezaei <saeed.rezaei1@gmail.com>
2021-10-24 15:34:36 +0330

    init better struct sample app

---
+++ betterstruct/src/components/Manager/Manager.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import'./Manager.css';
+
+const Manager = (props) => {
+    const assignedClasses = [];
+    let btnClass = '';
+    if (props.showPersons) {
+        btnClass = 'Red';
+    }
+    if (props.persons.length <= 2) {
+        assignedClasses.push('red'); // classes = ['red']
+    }
+    if (props.persons.length <= 1) {
+        assignedClasses.push('bold'); // classes = ['red', 'bold']
+    }
+
+    return (
+        <div className={'Manager'}>
+            <h1>Hi, I'm a React App</h1>
+            <p className={assignedClasses.join(' ')}>This is really working!</p>
+            <button
+                className={btnClass}
+                onClick={props.clicked}>Toggle Persons
+            </button>
+        </div>
+    );
+}
+
+export default Manager;
